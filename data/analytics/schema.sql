72a8b8816bda1154f5a62adfbf051a8cf3b9a2e7
domsoos <soos.domi@gmail.com>
2023-02-09 11:09:48 -0500

    updated sql and compose files

--- data/analytics/schema.sql
+++ data/analytics/schema.sql
@@ -1,37 +1,32 @@
 CREATE DATABASE IF NOT EXISTS `ridedb`;
 USE `ridedb`;
 
-CREATE TABLE Wizards (
+CREATE TABLE IF NOT EXISTS Wizards (
     wizardid int PRIMARY KEY,
     templateid int NULL,
     type varchar(50) NOT NULL
 );
-
-CREATE TABLE Template (
+CREATE TABLE IF NOT EXISTS Template (
     templateid int PRIMARY KEY,
-    eventid int NULL,
     type varchar(50) NOT NULL,
-    wizardid int NOT NULL,
+    wizardid int NOT NULL
 );
-
-CREATE TABLE Event (
-    shipmentid int PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS Events (
+    eventid int PRIMARY KEY,
     wizardid int NULL,
     templateid int NOT NULL,
     type varchar(50) NOT NULL,
     action varchar(50) NOT NULL,
-    actiondate datetime NOT NULL,
+    actiondate datetime NOT NULL
 );
-
-
-ALTER TABLE Event ADD FOREIGN KEY (wizardid) REFERENCES Wizards(wizardid);
-ALTER TABLE Event ADD FOREIGN KEY (templateid) REFERENCES Template(templateid);
+ALTER TABLE Events ADD FOREIGN KEY (wizardid) REFERENCES Wizards(wizardid);
+ALTER TABLE Events ADD FOREIGN KEY (templateid) REFERENCES Template(templateid);
 ALTER TABLE Wizards ADD FOREIGN KEY (templateid) REFERENCES Template(templateid);
 ALTER TABLE Template ADD FOREIGN KEY (wizardid) REFERENCES Wizards(wizardid);
-ALTER TABLE Template ADD FOREIGN KEY (eventid) REFERENCES Event(shipmentid);
+
+INSERT INTO Wizards(wizardid, templateid) VALUES(1000, 10000);
+INSERT INTO Template(templateid, eventid, type, wizardid) VALUES(10000,100,"button click",1000);
+INSERT INTO Events(eventid, wizardid, templateid, type, action, actiondate) VALUES(100, 1000,10000,"button click", "open wizard", NOW());
 
 
-INSERT INTO Event(shipmentid, wizardid, templateid, type, action, actiondate) VALUES(1, 1,1,"button click", "open wizard", NOW());
-INSERT INTO Wizards(wizardid, templateid) VALUES(1, 1);
-INSERT INTO Template(templateid, eventid, type, wizardid) VALUES(1,1,"button click",1);
 
