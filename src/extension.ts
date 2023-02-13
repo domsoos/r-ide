7496bc9270ab0c0b577b5510b0118ecdf47487f9
Daniel Koontz <dak.7997@gmail.com>
2023-02-13 17:04:21 -0500

    Created RosProject Object to track RosPacakges

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,5 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import { TextEncoder } from 'util';
 import * as vscode from 'vscode';
 import { SidebarBagsProvider } from './SidebarBagsProvider';
 import { SidebarTopicsProvider } from './SidebarTopicsProvider';
@@ -11,6 +10,10 @@ import {
 	createMessage,
 	createSrv
 } from './commands/commands';
+import { 
+	addMsgToProject, addSrvToProject, createRosProject, registerProject, RosProject
+} from './commands/RosProject';
+import { identity } from 'svelte/internal';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -22,6 +25,7 @@ export function activate(context: vscode.ExtensionContext) {
 	const sidebarTopicsProvider = new SidebarTopicsProvider(context.extensionUri);
 
 	context.subscriptions.push(
+		// Webviews
 		vscode.window.registerWebviewViewProvider(
 			"sidebar-wizards",
 			sidebarWizardsProvider
@@ -38,9 +42,17 @@ export function activate(context: vscode.ExtensionContext) {
 			"sidebar-topics",
 			sidebarTopicsProvider
 		),
+
+		// Commands
+
+		// Create templates and directories
 		vscode.commands.registerCommand(
 			"r-ide.create-file-from-template",
 			createFileFromTemplate
+		),
+		vscode.commands.registerCommand(
+			"r-ide.create-package",
+			createRosProject
 		),
 		vscode.commands.registerCommand(
 			"r-ide.create-msg", 
@@ -49,7 +61,43 @@ export function activate(context: vscode.ExtensionContext) {
 		vscode.commands.registerCommand(
 			"r-ide.create-srv",
 			createSrv
-		)
+		),
+
+		// Register files and projects
+		vscode.commands.registerCommand(
+			"r-ide.register-project",
+			registerProject
+		),
+		vscode.commands.registerCommand(
+			"r-ide.register-msg",
+			addMsgToProject
+		),
+		vscode.commands.registerCommand(
+			"r-ide.register-srv",
+			addSrvToProject
+		),
+
+		// Workspace listener
+		vscode.workspace.onDidChangeTextDocument((event) => {
+			switch (event.document.languageId) {
+				case ('python'): {
+					break;
+				}
+
+				case ('cpp'): {
+					break;
+				}
+
+				case ('ros.msg'): {
+					break;
+				}
+
+				default: {
+					console.log(event.document.languageId);
+					break;
+				}
+			}
+		}),
 	  );
 }
 
