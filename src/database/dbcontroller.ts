e59909f378850a59442feeb51b93837a46dbbfb7
Daniel Koontz <dak.7997@gmail.com>
2023-02-26 04:37:06 -0500

    all-hands demo

--- src/database/dbcontroller.ts
+++ src/database/dbcontroller.ts
@@ -5,8 +5,8 @@ require('./models/wizards');
 
 var mongoose = require('mongoose');
 var event = mongoose.model('Event');
-var template = mongoose.model('Template')
-var wizard = mongoose.model('Wizard')
+var template = mongoose.model('Template');
+var wizard = mongoose.model('Wizard');
 
 export async function connectToDB(){
     mongoose.connect("mongodb+srv://rideDB:nl3fP1uYDI9DW8Cx@cluster0.xroyxn3.mongodb.net/?retryWrites=true&w=majority");
@@ -25,6 +25,6 @@ export async function addNewEvent(desc: String, success: boolean){
     newEvent.save();
 }
 
-export async function retrieveTemplate(temp:String, success:boolean){
-    var template = 
-}
+// export async function retrieveTemplate(temp:String, success:boolean){
+//     var template = 
+// }
