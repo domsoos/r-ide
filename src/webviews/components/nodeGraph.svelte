802965f36315a33f8d4fb9850f601e85b3d4e147
Josh Peterson <Jpete020@odu.edu>
2023-04-03 19:27:03 -0400

    added nodegraph foundation

--- src/webviews/components/nodeGraph.svelte
+++ src/webviews/components/nodeGraph.svelte
@@ -3,6 +3,8 @@
     import { onMount } from 'svelte';
     import ROS from "../../ROSManagers/rosmanager"
     import ROSLIB from "roslib";
+    import { DataSet, Network } from "vis-network/standalone";
+    //import vis from "vis-network";
 
     let rosApi;
 
@@ -13,7 +15,6 @@
 
 
     onMount(async () => {
-
     try {
         await new ROS();
         rosApi = ROS.getROSApi();
@@ -66,7 +67,37 @@
                 console.log(res);
             })
         }
+
+        const mynodes = new DataSet([
+            { id: 1, label: "Node 1" },
+            { id: 2, label: "Node 2" },
+            { id: 3, label: "Node 3" },
+            { id: 4, label: "Node 4" },
+            { id: 5, label: "Node 5" }
+        ]);
+
+        const edges = new DataSet([
+            { from: 1, to: 3 },
+            { from: 1, to: 2 },
+            { from: 2, to: 4 },
+            { from: 2, to: 5 },
+            { from: 3, to: 3 }
+        ]);
+
+        const container = document.getElementById("mynetwork");
+        container.style.height = "600px";
+
+        const data = {
+            nodes: mynodes,
+            edges: edges
+        };
+
+        const options = {};
+        const network = new Network(container, data, options);
     }
 
 </script>
+
+
+<div id="mynetwork"></div>
 
